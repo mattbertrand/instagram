4d53a82c6312bac56b7c755432b78faf50f78b09
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-06-26 15:06:15 -0700

    formatting with prettier and eslint

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyCYndwTQHH8I33d5vW-W5zX9L3ALhsP9iQ',
+  authDomain: 'instagram-yt-48a56.firebaseapp.com',
+  projectId: 'instagram-yt-48a56',
+  storageBucket: 'instagram-yt-48a56.appspot.com',
+  messagingSenderId: '430149369282',
+  appId: '1:430149369282:web:909644cd19a2bc32342a5b'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+console.log('firebase', firebase);
+
+export { firebase, FieldValue };
